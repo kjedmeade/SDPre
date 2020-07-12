77d209a8f8370be92c3b0eaf71bfff32406830b1
Kayalyn <kjedmeade@gmail.com>
2020-07-12 16:48:01 -0700

    second commit

--- Labs/Objective1Lab2.java
+++ Labs/Objective1Lab2.java
@@ -5,6 +5,6 @@ public class Objective1Lab2 {
     System.out.println("| 1. Milk        |");
     System.out.println("| 2. Eggs        |");
     System.out.println("| 3. Bread       |");
-    System.out.println("----------------");
+    System.out.println("----------------") ;
   }
 }

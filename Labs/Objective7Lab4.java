527bfa095e1da6549fd71d8df86232ecab85511d
Kayalyn <kjedmeade@gmail.com>
2020-07-22 10:12:32 -0700

    first commit

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+
+    int count =1;
+    int sumtotal=0;
+
+while (count<=20){
+  sumtotal+=count;
+  count+=1;
+}
+System.out.println(sumtotal);
+}
+}

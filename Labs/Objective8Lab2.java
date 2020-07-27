ee16f93b86388462a3828896539ce44ee66ca66b
Kayalyn <kjedmeade@gmail.com>
2020-07-27 09:30:00 -0700

    first commit

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+
+    int sumtotal=0;
+
+for (int i=1; i<=20; i++){
+  sumtotal+=i;
+}
+System.out.println(sumtotal);
+}
+}

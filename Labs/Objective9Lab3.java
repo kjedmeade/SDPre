a7838021667bf2068bbab6cce06ea915dc500fbd
Kayalyn <kjedmeade@gmail.com>
2020-07-27 10:35:29 -0700

    first commit

---
+++ Labs/Objective9Lab3.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Objective9Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int selection;
+
+    while(true){
+
+      printMenu();
+
+      selection=scanner.nextInt();
+
+      if (selection==1){
+        System.out.println("Hello Human");
+        System.out.println("_____Menu_____");
+        System.out.println("1: Say Hello");
+        System.out.println("2: List My favorite foods");
+        System.out.println("3: Exit");
+        System.out.println();
+        }
+
+      else if (selection ==2){
+        System.out.println("Apples, Bananas,Coconuts");
+        System.out.println("_____Menu_____");
+        System.out.println("1: Say Hello");
+        System.out.println("2: List My favorite foods");
+        System.out.println("3: Exit");
+        System.out.println();
+        }
+
+      else {
+        System.out.println("Goodbye");
+        break;
+        }
+
+scanner.close();
+      }
+
+}
+
+
+  public static void printMenu() {
+    System.out.println("_____Menu_____");
+    System.out.println("1: Say Hello");
+    System.out.println("2: List My favorite foods");
+    System.out.println("3: Exit");
+    System.out.println();
+  }
+
+
+}
